c12028515507dc52490d41460b6c7ab005e1a7cf
cdebasa <cdebasa@teckpert.com>
2020-05-25 13:49:41 -0400

    changing in middleware_classes. Might be a conflict with a global version of django in my computer and the local version that I just created.

--- aviation/settings.py
+++ aviation/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
