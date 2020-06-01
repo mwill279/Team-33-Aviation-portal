d59e1fb5f54e722fb4069abe2842f5d5d13330d1
danyfranco <danyfranco14@yahoo.com>
2020-06-01 17:12:51 -0400

    users can update username/email and reset password

--- aviation-project/aviation/settings.py
+++ aviation-project/aviation/settings.py
@@ -134,3 +134,12 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# this is a gmail account I temporarily created for this project
+EMAIL_HOST_USER = 'aviationjobboard@gmail.com'
+EMAIL_HOST_PASSWORD = 'ajbpass1029'
