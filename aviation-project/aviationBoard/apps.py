296d08d55512d06a34f026497e2597cf27e4bac4
cdebasa <cdebasa@teckpert.com>
2020-06-02 12:06:53 -0400

    redownloading

--- aviation-project/aviationBoard/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AviationboardConfig(AppConfig):
-    name = 'aviationBoard'
--- aviation-project/aviationLogin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AviationloginConfig(AppConfig):
-    name = 'aviationLogin'
