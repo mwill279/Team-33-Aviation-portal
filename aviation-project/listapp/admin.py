1f7ccc0e3fadc4440d322c9f968b140b61c6ac4b
mwill279 <mwill279@fiu.edu>
2020-06-04 13:23:08 -0400

    listapp, use to edit and delete job

---
+++ aviation-project/listapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Job
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['jobID', 'title', 'jobtype', 'deadline']
