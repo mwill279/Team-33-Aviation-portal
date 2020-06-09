d03feeb8f0b2cd16ed544006e7d03c09411240a5
tzhan024 <60279540+tzhan024@users.noreply.github.com>
2020-06-08 23:03:36 -0400

    Add files via upload

---
+++ aviation-project/userProfile/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Users (models.Model): 
+	Email = models.CharField(max_length = 40)
+	Username = models.CharField(max_length = 20)
+	name = models.CharField(max_length = 40)
+	Address = models.CharField(max_length = 40)
+	phoneNumber = models.CharField(max_length = 10)
+	nickName = models.CharField(max_length = 20)
+	password = models.CharField(max_length = 40)
+	
+	
+class workExperience (models.Model):
+	job = models.CharField(max_length = 40)
+	years = models.CharField(max_length = 20)
+	company = models.CharField(max_length = 30)
+	comment = models.CharField(max_length = 150)
+	Username = models.CharField(max_length = 20)
+
+    
