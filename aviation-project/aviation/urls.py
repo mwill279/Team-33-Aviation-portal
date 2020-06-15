9ed85c019c4f3237ce6a6d085934b11ad824355a
Ryan P <ryanpineyro@gmail.com>
2020-06-15 15:59:48 -0400

    Fileupload Button
    Added file upload button and resume page
    
    Pyresparser is required for using the file upload button

--- aviation-project/aviation/urls.py
+++ aviation-project/aviation/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('jobpost/', postjob_view, name='post_job'),
     path('register/', user_views.register, name='register'),
+    path('resume/', user_views.resume, name='resume'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
--- aviation-project/users/views.py
+++ aviation-project/users/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm
-
+from django.core.files.storage import FileSystemStorage
+from pyresparser import ResumeParser
+from django.conf import settings
+import os
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -35,3 +38,18 @@ def profile(request):
     }
 
     return render(request, 'users/profile.html', context)
+@login_required()
+def resume(request):
+    parsed_info = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['resume']
+        if uploaded_file.name.endswith(".pdf") or uploaded_file.name.endswith(".docx"):
+            fs = FileSystemStorage(location = os.path.join(settings.MEDIA_ROOT, 'resumes'))
+            fs.save(uploaded_file.name, uploaded_file)
+            path = os.path.join(settings.MEDIA_ROOT, 'resumes') + '/'+ uploaded_file.name
+            parsed_info = ResumeParser(path).get_extracted_data()
+            print(parsed_info)
+            os.remove(path)
+        else:
+            print("Invalid Request")
+    return render(request, 'users/resume.html', parsed_info)
