bc5c8db49bc2fe9bf6b7bff5e7b3a3c2b2793831
mwill279 <mwill279@fiu.edu>
2020-07-23 21:26:19 -0400

    updated search.html

--- aviation-project/aviation/urls.py
+++ aviation-project/aviation/urls.py
@@ -21,47 +21,6 @@ from django.conf.urls.static import static
 """from listapp import views as listapp_views""" """listapp_views doesn't exist"""
 from postjob import views as postjob_views
 
-from aviation_job_board.views import home_view,portal_view, companypage_view, postjob_view, chatRoom_view
-from users import views as user_views
-from events_app.views import events_view
-urlpatterns = [
-    path('', home_view, name='home'),
-    path('portal/', portal_view, name='portal_view'),
-    path('company/',companypage_view, name='company_page'),
-    path('inbox/',chatRoom_view, name='inbox'),
-    path('admin/', admin.site.urls),
-    path('jobpost/', postjob_view, name='post_job'),
-    
-    # JOB SEARCH PATH
-    path('search/', postjob_views.searchpage, name='search_page'),
-    
-    path('events/', events_view, name='event_list'),
-    path('register/', user_views.register, name='register'),
-    path('appStatus/', user_views.applicationStatus_view, name='application_status'),
-    path('resume/', user_views.resume, name='resume'),
-    path('review/', user_views.review, name='review'),
-    path('profile/', user_views.jobseeker_profile_view, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    path('oauth/', include('social_django.urls', namespace='social')),
-    
-    
-    # JOB SEARCH PATHS
-    path('postjob/', postjob_views.posting, name='posting'),
-    path('jobsearch/', postjob_views.jobsearch, name='jobsearch'),
-    path('jobsearch/<int:job_id>/', postjob_views.job_detail, name='job_detail'),
-    
-    path('trysearch/', user_views.trysearch, name='trysearch'),
-    path('applyjob/', user_views.applyjob, name='applyjob'),
-   
-]
-
-from postjob import views as postjob_views
-
 from aviation_job_board.views import home_view, companypage_view, postjob_view, chooseRegister_view, chatRoom_view
 from users import views as user_views
 from events_app.views import events_view
@@ -73,7 +32,7 @@ urlpatterns = [
     path('jobpost/', postjob_views.posting, name='post_job'),
     
     # JOB SEARCH PATH
-    path('search/', postjob_views.searchpage, name='search_page'),
+    path('search/', postjob_views.jobsearch, name='search_page'),
     
     
     path('events/', events_view, name='event_list'),
