b2684f223573516e9480262878f30b4d2bd3a2c1
danyfranco <danyfranco14@yahoo.com>
2020-07-23 21:35:23 -0400

    added the update tab to company_profile

--- aviation-project/aviation/settings.py
+++ aviation-project/aviation/settings.py
@@ -129,10 +129,10 @@ WSGI_APPLICATION = 'aviation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'new_shit',
         'USER': 'postgres',
-        'PASSWORD': 'aviationpass', #Put the password that you used for PostrgeSQL
-        'HOST': 'ajbdb.cw3uaamzkbuc.us-east-2.rds.amazonaws.com',
+        'PASSWORD': 'danydb1270', #Put the password that you used for PostrgeSQL
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
